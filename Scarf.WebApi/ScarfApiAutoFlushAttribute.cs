8e63c8cd9a68feab4f465e69aecfea1ff0e9a45e
Lenard Gunda <lenard@rebuildall.fi>
2014-07-02 14:36:34 +0300

    Autflush for Api

---
+++ Scarf.WebApi/ScarfApiAutoFlushAttribute.cs
@@ -0,0 +1,27 @@
+﻿#region Copyright and license
+//
+// SCARF - Security Audit, Access and Action Logging
+// Copyright (c) 2014 ReBuildAll Solutions Ltd
+//
+// Author:
+//    Lenard Gunda 
+//
+// Licensed under MIT license, see included LICENSE file for details
+#endregion
+
+using System;
+using System.Web.Http.Filters;
+
+namespace Scarf.WebApi
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class ScarfAutoFlushAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+
+            ScarfLogging.CurrentContext.Commit();
+        }
+    }
+}
